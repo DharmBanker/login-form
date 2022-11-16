6c746bc3a41c66e82b170e82c2f19ed8a10d9e8f
dharm <dharmbanker123@gmail.com>
2022-11-16 20:03:27 +0530

    this is updated

---
+++ app.js
@@ -0,0 +1,18 @@
+// const button = document.querySelector('#clickMe');
+
+// function cliclEvent()
+// {
+//     const valueis = document.querySelectorAll('#click').value ;
+//     console.log(valueis);
+
+// }
+// button.addEventListener('clickMe',click);
+
+
+const button = document.querySelector('#clickMe');
+
+function event()
+{
+    alert('Button is click');
+}
+button.addEventListener("click",clicl);
